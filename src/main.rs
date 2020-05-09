39278f9136203a91c6f4e3f743978ccd25352793
Michael McClimon <michael@fastmailteam.com>
2020-05-09 09:14:00 -0400

    Add initial arg setup and help

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 mod config;
 mod merger;
 
+use clap::{crate_name, crate_version, App, Arg};
+
 use merger::Merger;
 
+const TEMPLATE: &str = "\
+{bin} {version} - {about}
+
+{usage}
+
+{unified}
+";
+
 fn main() {
-    let merger = Merger::from_config_file("config.toml");
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .about("build git branches from merge requests")
+        .template(TEMPLATE)
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .required(true)
+                .help("config file to use")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("auto")
+                .long("auto")
+                .help("do not run in interactive mode"),
+        )
+        .get_matches();
+
+    let merger = Merger::from_config_file(matches.value_of("config").unwrap());
     println!("{:?}", merger);
 }
